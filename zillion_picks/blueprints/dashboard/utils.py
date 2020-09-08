a6d68942a7ba8f320dd1cd61a80c92fa133fb3ae
gardnmi <gardnmi@gmail.com>
2020-09-07 20:12:36 -0500

    modified date format

--- zillion_picks/blueprints/dashboard/utils.py
+++ zillion_picks/blueprints/dashboard/utils.py
@@ -53,7 +53,8 @@ def table_cleanup(df, week):
     else:
         pass
 
-    df['start_date'] = pd.to_datetime(df['start_date']).dt.date
+    df['start_date'] = pd.to_datetime(
+        df['start_date']).dt.strftime('%B %d, %Y')
 
     df = df[cols]
     df = df.rename(columns=col_names)
